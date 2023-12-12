558d58c5fa9ffc2f422868821a608bb97f03b9b4
jaberabed <42500317+jaberabed@users.noreply.github.com>
2023-12-11 20:54:59 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace FinalTest2
+{
+   internal class Program
+   {
+      private static void Main(string[] args)
+      {
+         Console.WriteLine("Hello, World!");
+         ValueContainer<int> intContainer = new ValueContainer<int>();
+         intContainer.AddItem(10);
+         intContainer.AddItem(30);
+         intContainer.AddItem(80);
+         intContainer.AddItem(20);
+
+         Console.WriteLine("Descending Collection");
+         intContainer.DescendingCollection();
+      }
+
+      public class ValueContainer<T> where T : struct
+      {
+         public List<T> newList;
+
+         public ValueContainer()
+         {
+            newList = new List<T>();
+         }
+
+         public void AddItem(T item)
+         {
+            newList.Add(item);
+         }
+
+         public List<T> DescendingCollection()
+         {
+            List<T> sortedList = newList.OrderByDescending(x => x).ToList();
+            return sortedList;
+         }
+      }
+   }
+}
